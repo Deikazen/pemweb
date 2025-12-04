028e23d23516272a29b87ef87ab436d458b9b8cb
Satria <mfsmfsmfs35@gmail.com>
2025-12-04 22:10:41 +0700

    second commit

---
+++ app/Models/dosen.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Dosen extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tabel_dosen';
+
+    protected $fillable = [
+        'nama',
+        'nidn',
+        'jenis_kelamin',
+        'email',
+        'telepon',
+        'alamat'
+    ];
+}
---
+++ database/seeders/MahasiswaSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Mahasiswa;
+
+class MahasiswaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Mahasiswa::create([
+            'nama' => 'Budi Santoso',
+            'nim' => '12345678',
+            'email' => 'budi@gmail.com',
+            'jurusan' => '15',
+            'dosenwali' => '11837',
+            'created_at' => date('Y-m-d H:i:s')
+        ]);
+    }
+}
